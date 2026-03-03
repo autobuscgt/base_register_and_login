b8ea665980f07629d4521ff7bb692c52dabaa9f1
JohnDoe <vovani4egonepishet@yandex.ru>
2026-03-03 13:26:25 +0300

    proccess env

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = 'akjsfklafs';
+const secret = process.env.SECRET_KEY;
 module.exports = function(req,res,next){
     if(req.method === 'OPTIONS'){
         next()
@@ -7,11 +7,11 @@ module.exports = function(req,res,next){
     if (!req.headers.authorization) {
         return res.status(401).json({ message: 'Нет заголовка авторизации' });
     }
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(' ')[1]; //  tokenaskfakl.sfklasklfkaklsfklaklfk.laklsfklaklsfkl
     if(!token){
         return res.send('no auth')
     }
-    const decoded = jwt.verify(token,SECRET_KEY);
+    const decoded = jwt.verify(token,secret);
     if(!decoded){
         return res.send('no auth')
     }
--- middleware/checkRolemiddleware.js
+++ middleware/checkRolemiddleware.js
@@ -1,33 +1,25 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = 'akjsfklafs';
+const secret = process.env.SECRET_KEY;
 
 module.exports = function(role){
     return function(req, res, next){
-
         if(req.method === "OPTIONS"){
             return next();
         }
-
         if (!req.headers.authorization) {
             return res.status(401).json({ message: 'Нет заголовка авторизации' });
         }
-        
         try {
             const token = req.headers.authorization.split(' ')[1];
-            
             if(!token){
                 return res.status(401).json({ message: 'Токен не предоставлен' });
             }
-            
-            const decoded = jwt.verify(token, SECRET_KEY);
-            
+            const decoded = jwt.verify(token, secret);
             if(decoded.role !== role){
                 return res.status(403).json({ message: 'Нет доступа' });
             }
-            
             req.user = decoded;
-            next();
-            
+            next();  
         } catch (error) {
             return res.status(401).json({ message: 'Ошибка авторизации' });
         }
